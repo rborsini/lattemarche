240f967d9d100e75d1b292e8eb72bef84021873c
Roberto Borsini <r.borsini@loccioni.com>
2017-09-12 11:38:16 +0200

    layout refactory

--- LatteMarche.WebApi/Controllers/HomeController.cs
+++ LatteMarche.WebApi/Controllers/HomeController.cs
@@ -19,5 +19,6 @@ namespace LatteMarche.WebApi.Controllers
 
             return View();
         }
+
     }
 }
---
+++ LatteMarche.WebApi/Controllers/UtentiController.cs
@@ -0,0 +1,41 @@
+﻿using LatteMarche.WebApi.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace LatteMarche.WebApi.Controllers
+{
+    public class UtentiController : Controller
+    {
+        public UtentiController()
+        {
+        }
+
+        // GET: Utenti
+        [ViewItem(nameof(Index), "Utenti", "Lista")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Utente/New
+        [ViewItem(nameof(New), "Utenti", "Aggiungi")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Utente/Details
+        [ViewItem(nameof(Details), "Utenti", "Modifica")]
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
