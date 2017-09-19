05f541c11b778c3023d0e21e04bf57707c833b3a
Giacomo Corallini <corallinigiacomo89@gmail.com>
2017-09-19 12:28:21 +0200

    Vommito la creazione e il front-end della pagina di log in

---
+++ LatteMarche.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using LatteMarche.WebApi.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace LatteMarche.WebApi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
