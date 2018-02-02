bc886a80784417d5c2bdef55a56e81c53e211f4e
Roberto Borsini <roberto.borsini@gmail.com>
2018-02-02 10:22:32 +0100

    Aggiunto lattemarche service

---
+++ LatteMarche.Service/Jobs/SynchJob.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LatteMarche.Service.Jobs
+{
+    public class SynchJob : BaseJob
+    {
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        public SynchJob()
+            : base() { }
+
+        #endregion
+
+        #region Methods
+
+        public override void Execute()
+        {
+            this.log.Debug("Debug synch job");
+            this.log.Info("Info synch job");
+
+            System.Threading.Thread.Sleep(6000);
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ LatteMarche.Service/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topshelf;
+
+namespace LatteMarche.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceStartUp>(s =>
+                {
+                    s.ConstructUsing(name => new ServiceStartUp());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("LatteMarche.Service");
+                x.SetDisplayName("LatteMarche.Service");
+                x.SetServiceName("LatteMarche.Service");
+            });
+        }
+    }
+}
