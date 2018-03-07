8a7e1ec0c6b58bfc23fac31f67b47fef5ce1f1ca
Daniele Dignani <daniele.dignani@me.com>
2018-03-07 12:11:34 +0100

    synch with database

---
+++ LatteMarche.EntityFramework/Migrations/201803071110060_Add_column_fattoreConversione_on_TipiLatte.cs
@@ -0,0 +1,18 @@
+namespace LatteMarche.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_column_fattoreConversione_on_TipiLatte : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TIPO_LATTE", "FATTORE_CONVERSIONE", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.TIPO_LATTE", "FATTORE_CONVERSIONE");
+        }
+    }
+}
