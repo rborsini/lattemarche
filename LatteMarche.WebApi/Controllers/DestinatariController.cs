78e3122d70a49e8b800d1d7b943b5bbffafc5476
Giacomo Corallini <corallinigiacomo89@gmail.com>
2018-06-29 10:54:21 +0200

    nuove view e aggiunte al menu in modo statico

---
+++ LatteMarche.WebApi/Controllers/DestinatariController.cs
@@ -0,0 +1,21 @@
+﻿using LatteMarche.WebApi.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.UI;
+
+namespace LatteMarche.WebApi.Controllers
+{
+    public class DestinatariController : Controller
+    {
+        [OutputCache(Duration = 3600, VaryByParam = "none", Location = OutputCacheLocation.Client, NoStore = true)]
+        public ActionResult Index()
+        {           
+            return View();
+        }
+
+    }
+}
