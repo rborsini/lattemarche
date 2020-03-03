e02366170105995b5fb12552d06c1fd67d8c8a34
Roberto Borsini <roberto.borsini@gmail.com>
2020-03-03 12:14:03 +0100

    #325694

---
+++ LatteMarche.AssamParserConsole/Models/Misura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatteMarche.AssamParserConsole.Models
+{
+    public class Misura
+    {
+        public string Nome { get; set; }
+        public string Uom { get; set; }
+
+        public string Valore { get; set; }
+
+        public bool FuoriSoglia { get; set; }
+    }
+}
---
+++ LatteMarche.AssamParserConsole/Models/Report.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatteMarche.AssamParserConsole.Models
+{
+    public class Report
+    {
+        public string Committente { get; set; }
+
+        public string Produttore_Codice { get; set; }
+
+        public string Produttore_Nome { get; set; }
+
+        public string TipoLatte { get; set; }
+
+        public List<AnalisiLatte> Analisi { get; set; }
+
+        public Report()
+        {
+            this.Analisi = new List<AnalisiLatte>();
+        }
+
+    }
+}
