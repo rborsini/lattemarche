537807b3498491ffd895ecdc30e96a6721e14872
Roberto Borsini <roberto.borsini@gmail.com>
2020-03-17 10:33:49 +0100

    implementato master details con dati mockati

--- LatteMarche.Xamarin/App.xaml.cs
+++ LatteMarche.Xamarin/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LatteMarche.Xamarin.Services;
+using LatteMarche.Xamarin.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,6 +12,7 @@ namespace LatteMarche.Xamarin
         {
             InitializeComponent();
 
+            DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
         }
 
---
+++ LatteMarche.Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using LatteMarche.Xamarin.Models;
+using LatteMarche.Xamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LatteMarche.Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
