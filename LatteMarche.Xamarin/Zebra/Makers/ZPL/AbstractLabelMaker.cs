97fed81531c2c54452309074ebd3108fc7f18741
Giacomo Corallini <corallinigiacomo89@gmail.com>
2020-05-11 18:16:23 +0200

    commit parziale

--- LatteMarche.Xamarin/Zebra/Makers/ZPL/AbstractLabelMaker.cs
+++ LatteMarche.Xamarin/Zebra/Makers/ZPL/AbstractLabelMaker.cs
@@ -8,6 +8,27 @@ namespace LatteMarche.Xamarin.Zebra.Makers.ZPL
 {
     public abstract class AbstractLabelMaker : ILabelMaker
     {
+        protected int leftOffset = 50;
+        protected int h2 = 22;
+        protected int h6 = 12;
+        public string start_print = "^XA";
+        public string end_print = "^XZ";
+
         public abstract string MakeLabel(Registro registro);
+
+
+
+        protected string MakeHeader()
+        {
+            var header_line_1 = $"^CFA,{h2}^FO{leftOffset},50^FDLatte Marche^FS";
+            var header_line_2 = $"^CFA,{h6}^FO{leftOffset},80^FDOrganizzazione Produttori^FS";
+            return header_line_1 + header_line_2;
+        }        
+
+        protected string MakeLine()
+        {
+            var cmd = $"^FO{leftOffset},200^GB700,1,1^FS";
+            return cmd;
+        }
     }
 }
