f6a2e45dd10956b288735dc939875b3c2d44e328
Giacomo Corallini <corallinigiacomo89@gmail.com>
2020-05-26 15:18:47 +0200

    parziale sulla pagina trasportatori

--- lattemarche-vue/src/pages/trasportatori/edit.vue
+++ lattemarche-vue/src/pages/trasportatori/edit.vue
@@ -1,108 +1,110 @@
 <template>
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Dettaglio giro</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body pl-5 pr-5">
-                    <div v-if="progressBar">
-                        <div class="row">
-                            <div class="col-sm-8 offset-2">
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated"
-                                         role="progressbar" aria-valuenow="100"
-                                         aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
-                                </div>
-                                <h3 class="text-center pt-2">Elaborazione in corso...</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div v-else>
-                        <div class="row form-group">
-                            <label class="col-2">Codice</label>
-                            <div class="col-10">
-                                <input type="text" class="form-control" v-model="giro.CodiceGiro">
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <label class="col-2">Denominazione</label>
-                            <div class="col-10">
-                                <input type="text" class="form-control" v-model="giro.Denominazione">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="modal-footer">
-                    <button class="btn btn-secondary mr-2" data-dismiss="modal">Annulla</button>
-                    <button v-on:click="onSave()" class="btn btn-success">Salva</button>
-                </div>
-            </div>
+  <div
+    class="modal fade bd-example-modal-lg"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="myLargeModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Dettaglio giro</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
+        <div class="modal-body pl-5 pr-5">
+          <div v-if="progressBar">
+            <div class="row">
+              <div class="col-sm-8 offset-2">
+                <div class="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated"
+                    role="progressbar"
+                    aria-valuenow="100"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style="width: 100%"
+                  ></div>
+                </div>
+                <h3 class="text-center pt-2">Elaborazione in corso...</h3>
+              </div>
+            </div>
+          </div>
+          <div v-else>
+            <div class="row form-group">
+              <label class="col-2">Codice</label>
+              <div class="col-10">
+                <input type="text" class="form-control" v-model="giro.CodiceGiro" />
+              </div>
+            </div>
+            <div class="row form-group">
+              <label class="col-2">Denominazione</label>
+              <div class="col-10">
+                <input type="text" class="form-control" v-model="giro.Denominazione" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button class="btn btn-secondary mr-2" data-dismiss="modal">Annulla</button>
+          <button v-on:click="onSave()" class="btn btn-success">Salva</button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop, Watch, Emit } from "vue-property-decorator";
+import { Giro } from "../../models/giro.model";
+import { GiriService } from "../../services/giri.service";
 
-    import Vue from "vue";
-    import Component from "vue-class-component";
-    import { Prop, Watch, Emit } from "vue-property-decorator";
-    import { Giro } from "../../models/giro.model";
-    import { GiriService } from "../../services/giri.service";
+@Component({
+  components: {}
+})
+export default class GiroTrasportatoriModal extends Vue {
+  @Prop()
+  giro!: Giro;
 
-    @Component({
-        components: {}
-    })
+  public giriService: GiriService;
+  public progressBar: boolean = false;
 
-    export default class GiroTrasportatoriModal extends Vue {
+  constructor() {
+    super();
+    this.giriService = new GiriService();
+    this.giro = new Giro();
+  }
 
-        @Prop()
-        giro: Giro;
+  mounted() {}
 
-        public giriService: GiriService;
-        public progressBar: boolean = false;
+  public onSave() {
+    this.progressBar = true;
+    this.giriService.save(this.giro).then(response => {
+      if (response.data != undefined) {
+        this.progressBar = false;
+        this.close();
+      } else {
+        this.giro = response.data;
+      }
+    });
+  }
 
-        constructor() {
-            super();
-            this.giriService = new GiriService();
-            this.giro = new Giro();
-        }
+  public open(): void {
+    $(this.$el).modal("show");
+  }
 
-        mounted() {
-            
-        }
+  public openGiro(giro: Giro): void {
+    this.giro = giro;
+    this.open();
+  }
 
-        public onSave() {
-            this.progressBar = true;
-            this.giriService.save(this.giro)
-                .then(response => {
-                    if (response.data != undefined) {
-                        this.progressBar = false;
-                        this.close();
-                    } else {
-                        this.giro = response.data;
-                    }
-                })
-        }
-
-        public open(): void {
-            $(this.$el).modal('show');
-        }
-
-        public openGiro(giro: Giro): void {
-            this.giro = giro;
-            this.open();
-        }
-
-        public close(): void {
-            $(this.$el).modal('hide');
-        }
-
-
-    }
-
+  public close(): void {
+    $(this.$el).modal("hide");
+  }
+}
 </script>
