2ed33b53b3c4e25ef596e500fd72702116d85579
Roberto Borsini <roberto.borsini@gmail.com>
2020-05-26 08:20:19 +0200

    aggiunto endpoint widgets

---
+++ LatteMarche.WebApi/Controllers_Api/WidgetsController.cs
@@ -0,0 +1,88 @@
+﻿
+using LatteMarche.Application.Dashboard.Interfaces;
+using LatteMarche.Application.Utenti.Interfaces;
+using LatteMarche.WebApi.Attributes;
+using LatteMarche.WebApi.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace LatteMarche.WebApi.Controllers_Api
+{
+    [ApiCustomAuthorize]
+    [ApiActionFilter]
+    [ApiExceptionFilter]
+    public class WidgetsController : ApiController
+    {
+
+        #region Fields
+
+        private IWidgetsService widgetsService;
+        private IUtentiService utentiService;
+
+        #endregion
+
+        #region Constructor
+
+        public WidgetsController(IWidgetsService widgetsService, IUtentiService utentiService)
+        {
+            this.widgetsService = widgetsService;
+            this.utentiService = utentiService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [ViewItem(nameof(Sommario), "Widgets", "Sommario")]
+        [HttpGet]
+        public IHttpActionResult Sommario()
+        {
+            try
+            {
+                var user = this.utentiService.Details(User.Identity.Name);
+                return Ok(this.widgetsService.WidgetSommario(user.Id));
+            }
+            catch (Exception exc)
+            {
+                return InternalServerError(exc);
+            }
+        }
+
+        [ViewItem(nameof(Acquirenti), "Widgets", "Acquirenti")]
+        [HttpGet]
+        public IHttpActionResult Acquirenti()
+        {
+            try
+            {
+                var user = this.utentiService.Details(User.Identity.Name);
+                return Ok(this.widgetsService.WidgetAcquirenti(user.Id));
+            }
+            catch (Exception exc)
+            {
+                return InternalServerError(exc);
+            }
+        }
+
+        [ViewItem(nameof(TipiLatte), "Widgets", "TipiLatte")]
+        [HttpGet]
+        public IHttpActionResult TipiLatte()
+        {
+            try
+            {
+                var user = this.utentiService.Details(User.Identity.Name);
+                return Ok(this.widgetsService.WidgetTipiLatte(user.Id));
+            }
+            catch (Exception exc)
+            {
+                return InternalServerError(exc);
+            }
+        }
+
+        #endregion
+
+
+    }
+}
