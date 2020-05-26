204b727f1894e94406bb1d6c1119f64452b3a9cc
Giacomo Corallini <corallinigiacomo89@gmail.com>
2020-05-26 15:11:00 +0200

    fix tipilatte

--- lattemarche-vue/src/pages/tipiLatte/edit.vue
+++ lattemarche-vue/src/pages/tipiLatte/edit.vue
@@ -1,146 +1,146 @@
 <template>
-    <div class="modal fade bd-example-modal-lg" id="editazione-tipoLatte-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Dettagli tipo latte</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body pl-5 pr-5">
-
-                    <!--Descrizione-->
-                    <div class="row form-group">
-                        <label class="col-3">Descrizione</label>
-                        <div class="col-9">
-                            <input type="text" class="form-control" v-model="tipoLatte.Descrizione" />
-                        </div>
-                    </div>
-
-                    <!--Descrizione breve -->
-                    <div class="row form-group">
-                        <label class="col-3">Sigla</label>
-                        <div class="col-9">
-                            <input type="text" class="form-control" v-model="tipoLatte.DescrizioneBreve" />
-                        </div>
-                    </div>
-
-                    <!-- Fattore conversione -->
-                    <div class="row form-group">
-                        <label class="col-3">Fattore di conversione</label>
-                        <div class="col-9">
-                            <input type="number" step="0.001" class="form-control" v-model="tipoLatte.FattoreConversione" />
-                            <small class="form-text text-muted">Conversione da litri a kg.</small>
-                        </div>
-                    </div>
-
-                    <!-- Flag invio sitra -->
-                    <div class="row form-group">
-                        <label class="col-3"></label>
-                        <div class="col-9">
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="checkboxSitra" v-model="tipoLatte.FlagInvioSitra">
-                                <label class="form-check-label" for="checkboxSitra">Sincronizza con Sitra</label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- progress bar -->
-                    <div class="row" v-if="progressBarVisible">
-                        <div class="col-sm-4 offset-4 pt-2">
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated"
-                                     role="progressbar"
-                                     aria-valuenow="100"
-                                     aria-valuemin="0"
-                                     aria-valuemax="100"
-                                     style="width: 100%"></div>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 offset-4 text-center pt-2">
-                            <h4>Elaborazione in corso...</h4>
-                        </div>
-                    </div>
-                </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary mr-2" data-dismiss="modal">Annulla</button>
-                        <button class="btn btn-primary" v-on:click="onSave()">Salva</button>
-                    </div>
-                </div>
+  <div
+    class="modal fade bd-example-modal-lg"
+    id="editazione-tipoLatte-modal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="myLargeModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Dettagli tipo latte</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
+        <div class="modal-body pl-5 pr-5">
+          <!--Descrizione-->
+          <div class="row form-group">
+            <label class="col-3">Descrizione</label>
+            <div class="col-9">
+              <input type="text" class="form-control" v-model="tipoLatte.Descrizione" />
+            </div>
+          </div>
+
+          <!--Descrizione breve -->
+          <div class="row form-group">
+            <label class="col-3">Sigla</label>
+            <div class="col-9">
+              <input type="text" class="form-control" v-model="tipoLatte.DescrizioneBreve" />
+            </div>
+          </div>
+
+          <!-- Fattore conversione -->
+          <div class="row form-group">
+            <label class="col-3">Fattore di conversione</label>
+            <div class="col-9">
+              <input
+                type="number"
+                step="0.001"
+                class="form-control"
+                v-model="tipoLatte.FattoreConversione"
+              />
+              <small class="form-text text-muted">Conversione da litri a kg.</small>
+            </div>
+          </div>
+
+          <!-- Flag invio sitra -->
+          <div class="row form-group">
+            <label class="col-3"></label>
+            <div class="col-9">
+              <div class="form-group form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="checkboxSitra"
+                  v-model="tipoLatte.FlagInvioSitra"
+                />
+                <label class="form-check-label" for="checkboxSitra">Sincronizza con Sitra</label>
+              </div>
+            </div>
+          </div>
+
+          <!-- progress bar -->
+          <div class="row" v-if="progressBarVisible">
+            <div class="col-sm-4 offset-4 pt-2">
+              <div class="progress">
+                <div
+                  class="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="100"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style="width: 100%"
+                ></div>
+              </div>
+            </div>
+            <div class="col-sm-4 offset-4 text-center pt-2">
+              <h4>Elaborazione in corso...</h4>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary mr-2" data-dismiss="modal">Annulla</button>
+          <button class="btn btn-primary" v-on:click="onSave()">Salva</button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop, Watch, Emit } from "vue-property-decorator";
+import Select2 from "../../components/select2.vue";
 
-    import Vue from "vue";
-    import Component from "vue-class-component";
-    import { Prop, Watch, Emit } from "vue-property-decorator";
-    import Select2 from "../../components/select2.vue";
+import { Dropdown, DropdownItem } from "../../models/dropdown.model";
+import { Autocisterna } from "../../models/autocisterna.model";
+import { TipoLatte } from "../../models/tipoLatte.model";
 
-    import { Dropdown, DropdownItem } from "../../models/dropdown.model";
-    import { Autocisterna } from "../../models/autocisterna.model";
-    import { TipoLatte } from "../../models/tipoLatte.model";
+import { TipiLatteService } from "../../services/tipiLatte.service";
 
-    import { TipiLatteService } from "../../services/tipiLatte.service";
+@Component({
+  components: {
+    Select2
+  }
+})
+export default class EditazioneTipoLatteModal extends Vue {
+  @Prop()
+  tipoLatte!: TipoLatte;
 
-    @Component({
-        components: {
-            Select2
-        }
-    })
+  public tipiLatteService: TipiLatteService;
 
-    export default class EditazioneTipoLatteModal extends Vue {
+  public progressBarVisible = false;
 
-        @Prop()
-        tipoLatte!: TipoLatte;
+  constructor() {
+    super();
+    this.tipiLatteService = new TipiLatteService();
+  }
 
-        public tipiLatteService: TipiLatteService;
+  mounted() {}
 
-        public progressBarVisible = false;
+  public open(): void {
+    $(this.$el).modal("show");
+  }
 
+  public onSave() {
+    this.progressBarVisible = true;
+    this.tipiLatteService.save(this.tipoLatte).then(response => {
+      if (response.data != undefined) {
+        this.progressBarVisible = false;
+        this.close();
+      } else {
+        this.tipoLatte = response.data;
+        this.close();
+      }
+    });
+  }
 
-        constructor() {
-            super();
-            this.tipiLatteService = new TipiLatteService();
-        }
-
-        mounted() {
-
-
-        }
-
-        public open(): void {
-            $(this.$el).modal('show');
-        }
-
-
-        public onSave() {
-            this.progressBarVisible = true;
-            this.tipiLatteService.save(this.tipoLatte)
-                .then(response => {
-                    if (response.data != undefined) {
-                        this.$emit("salvato");
-                        this.progressBarVisible = false;
-                        this.close();
-                    } else {
-                        // save KO!!
-                        this.tipoLatte = response.data;
-                        // TODO: msg di validazione
-                        //this.$emit("errore");
-                        this.close();
-                    }
-                });
-        }
-
-
-
-        public close(): void {
-            $(this.$el).modal('hide');
-        }
-
-
-    }
-
+  public close(): void {
+    $(this.$el).modal("hide");
+  }
+}
 </script>
--- lattemarche-vue/src/pages/tipiLatte/index.vue
+++ lattemarche-vue/src/pages/tipiLatte/index.vue
@@ -1,212 +1,195 @@
 <template>
-    
+  <div>
+    <!-- waiter -->
+    <waiter ref="waiter"></waiter>
 
-    <div id="tipilatte-page" class="container-fluid p-0">
+    <!-- Pannello editazione dettaglio -->
+    <editazione-tipo-latte-modal ref="editazioneTipoLatteModal" :tipo-latte="tipoLatte"></editazione-tipo-latte-modal>
 
-        <!-- Pannello editazione dettaglio -->
-        <editazione-tipo-latte-modal ref="editazioneTipoLatteModal"
-                                    :tipo-latte="tipoLatte"
-                                    v-on:salvato="$refs.savedDialog.open()"></editazione-tipo-latte-modal>
+    <!-- Pannello notifica salvatagggio -->
+    <notification-dialog
+      ref="savedDialog"
+      :title="'Conferma salvataggio'"
+      :message="'Tipo latte salvato correttamente'"
+      v-on:ok="window.location = '/TipiLatte'"
+    ></notification-dialog>
 
-        <!-- Pannello notifica salvatagggio -->
-        <notification-dialog ref="savedDialog"
-                            :title="'Conferma salvataggio'"
-                            :message="'Tipo latte salvato correttamente'"
-                            v-on:ok="window.location = '/TipiLatte'"></notification-dialog>
+    <!-- Pannello notifica rimozione -->
+    <notification-dialog
+      ref="removedDialog"
+      :title="'Conferma rimozione'"
+      :message="'Tipo latte rimosso correttamente'"
+      v-on:ok="window.location = '/TipiLatte'"
+    ></notification-dialog>
 
-        <!-- Pannello notifica rimozione -->
-        <notification-dialog ref="removedDialog"
-                            :title="'Conferma rimozione'"
-                            :message="'Tipo latte rimosso correttamente'"
-                            v-on:ok="window.location = '/TipiLatte'"></notification-dialog>
+    <!-- Pannello modale conferma eliminazione -->
+    <confirm-dialog
+      ref="confirmDeleteDialog"
+      :title="'Conferma eliminazione'"
+      :message="'Sei sicuro di voler rimuovere il tipo latte selezionato?'"
+      v-on:confirmed="onRemove()"
+    ></confirm-dialog>
 
-        <!-- Pannello modale conferma eliminazione -->
-        <confirm-dialog ref="confirmDeleteDialog"
-                        :title="'Conferma eliminazione'"
-                        :message="'Sei sicuro di voler rimuovere il tipo latte selezionato?'"
-                        v-on:confirmed="onRemove()"></confirm-dialog>
+    <!-- Tabella -->
+    <data-table :options="tableOptions" :rows="tipiLatte" v-on:data-loaded="onDataLoaded">
+      <!-- Toolbox -->
+      <template slot="toolbox" v-if="canAdd">
+        <button class="toolbox btn btn-primary float-right" v-on:click="onAdd()">Aggiungi</button>
+      </template>
 
-        <!-- Tabella -->
-        <data-table :options="tableOptions" :rows="tipiLatte" v-on:data-loaded="onDataLoaded">
-
-            <!-- Toolbox -->
-            <template slot="toolbox" v-if="canAdd">
-                <button class="toolbox btn btn-primary float-right" v-on:click="onAdd()">Aggiungi</button>
-            </template>
-
-            <!-- Colonne -->
-            <template slot="thead">
-                <th>#</th>
-                <th>Descrizione</th>
-                <th>Sigla</th>
-                <th>Fattore conversione LT -> KG</th>
-                <th v-if="canEdit || canRemove"></th>
-            </template>
-
-        </data-table>
-    </div>
-
+      <!-- Colonne -->
+      <template slot="thead">
+        <th>#</th>
+        <th>Descrizione</th>
+        <th>Sigla</th>
+        <th>Fattore conversione LT -> KG</th>
+        <th v-if="canEdit || canRemove"></th>
+      </template>
+    </data-table>
+  </div>
 </template>
 <script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop, Watch, Emit } from "vue-property-decorator";
 
-    import Vue from "vue";
-    import Component from "vue-class-component";
-    import { Prop, Watch, Emit } from "vue-property-decorator";
+import DataTable from "../../components/dataTable.vue";
+import EditazioneTipoLatteModal from "../tipiLatte/edit.vue";
+import Waiter from "../../components/waiter.vue";
+import NotificationDialog from "../../components/notificationDialog.vue";
+import ConfirmDialog from "../../components/confirmDialog.vue";
 
-    import DataTable from "../../components/dataTable.vue";
-    import EditazioneTipoLatteModal from "../tipiLatte/edit.vue";
-    import NotificationDialog from "../../components/notificationDialog.vue";
-    import ConfirmDialog from "../../components/confirmDialog.vue";
+import { TipoLatte } from "../../models/tipoLatte.model";
+import { TipiLatteService } from "../../services/tipiLatte.service";
 
-    import { TipoLatte } from "../../models/tipoLatte.model";
-    import { TipiLatteService } from "../../services/tipiLatte.service";
+declare module "vue/types/vue" {
+  interface Vue {
+    open(): void;
+    close(): void;
+  }
+}
 
+@Component({
+  components: {
+    ConfirmDialog,
+    NotificationDialog,
+    EditazioneTipoLatteModal,
+    DataTable,
+    Waiter
+  }
+})
+export default class TipiLatteIndexPage extends Vue {
+  $refs: any = {
+    savedDialog: Vue,
+    editazioneTipoLatteModal: Vue,
+    confirmDeleteDialog: Vue,
+    removedDialog: Vue,
+    waiter: Vue
+  };
 
-    declare module 'vue/types/vue' {
-        interface Vue {
-            open(): void
-            close(): void
-        }
+  private tipiLatteService: TipiLatteService;
+  public tipoLatte: TipoLatte;
+  private idTipoLatte!: number;
+
+  public tableOptions: any = {};
+  public tipiLatte: TipoLatte[] = [];
+  public canAdd: boolean = false;
+  public canEdit: boolean = false;
+  public canRemove: boolean = false;
+
+  constructor() {
+    super();
+
+    this.tipiLatteService = new TipiLatteService();
+    this.tipoLatte = new TipoLatte();
+
+    this.canAdd = $("#canAdd").val() == "true";
+    this.canEdit = $("#canEdit").val() == "true";
+    this.canRemove = $("#canRemove").val() == "true";
+  }
+
+  public mounted() {
+    this.$refs.waiter.open();
+    this.initTable();
+
+    this.tipiLatteService.index().then(response => {
+      this.tipiLatte = response.data;
+      this.$refs.waiter.close();
+    });
+  }
+
+  // Evento fine generazione tabella
+  public onDataLoaded() {
+    $(".edit").click(event => {
+      var element = $(event.currentTarget);
+      var rowId = $(element).data("row-id");
+
+      this.tipiLatteService.details(rowId).then(response => {
+        this.tipoLatte = response.data;
+        this.$refs.editazioneTipoLatteModal.open();
+      });
+    });
+
+    $(".delete").click(event => {
+      var element = $(event.currentTarget);
+      this.idTipoLatte = $(element).data("row-id");
+
+      this.$refs.confirmDeleteDialog.open();
+    });
+  }
+
+  // nuova tipoLatte
+  public onAdd() {
+    this.tipoLatte = new TipoLatte();
+    this.$refs.editazioneTipoLatteModal.open();
+  }
+
+  // rimozione tipoLatte
+  public onRemove() {
+    this.tipiLatteService.delete(this.idTipoLatte).then(response => {
+      this.$refs.removedDialog.open();
+    });
+  }
+
+  // inizializzazione tabella
+  private initTable(): void {
+    var options: any = {};
+    options.columns = [];
+
+    options.columns.push({ data: "Id" });
+    options.columns.push({ data: "Descrizione" });
+    options.columns.push({ data: "DescrizioneBreve" });
+    options.columns.push({ data: "FattoreConversione" });
+
+    var ce = this.canEdit;
+    var cr = this.canRemove;
+
+    if (ce || cr) {
+      options.columns.push({
+        render: function(data: any, type: any, row: any) {
+          var html = '<div class="text-center">';
+
+          if (ce)
+            html +=
+              '<a class="edit" title="modifica" style="cursor: pointer;" data-row-id="' +
+              row.Id +
+              '" ><i class="far fa-edit"></i></a>';
+
+          if (cr)
+            html +=
+              '<a class="pl-3 delete" title="elimina" style="cursor: pointer;" data-row-id="' +
+              row.Id +
+              '" ><i class="far fa-trash-alt"></i></a>';
+
+          html += "</div>";
+
+          return html;
+        },
+        className: "edit-column",
+        orderable: false
+      });
     }
-
-    @Component({
-        el: '#tipilatte-page',
-        components: {
-            ConfirmDialog,
-            NotificationDialog,
-            EditazioneTipoLatteModal,
-            DataTable
-        }
-    })
-
-
-    export default class TipiLatteIndexPage extends Vue {
-
-
-        $refs: any = {
-            savedDialog: Vue,
-            editazioneTipoLatteModal: Vue,
-            confirmDeleteDialog: Vue,
-            removedDialog: Vue
-        }
-
-        private tipiLatteService: TipiLatteService;
-        public tipoLatte: TipoLatte;
-        private idTipoLatte!: number;
-
-        public tableOptions: any = {};
-        public tipiLatte: TipoLatte[] = [];
-        public canAdd: boolean = false;
-        public canEdit: boolean = false;
-        public canRemove: boolean = false;
-
-        constructor() {
-            super();
-
-            this.tipiLatteService = new TipiLatteService();
-            this.tipoLatte = new TipoLatte();
-
-            this.canAdd = $('#canAdd').val() == "true";
-            this.canEdit = $('#canEdit').val() == "true";
-            this.canRemove = $('#canRemove').val() == "true";
-        }
-
-        public mounted() {
-            this.initTable();
-
-            this.tipiLatteService.index()
-                .then(response => {
-                    this.tipiLatte = response.data;
-                });
-        }
-
-        // Evento fine generazione tabella
-        public onDataLoaded() {
-
-            $('.edit').click((event) => {
-
-                var element = $(event.currentTarget);
-                var rowId = $(element).data("row-id");
-
-                this.tipiLatteService.details(rowId)
-                    .then(response => {
-                        this.tipoLatte = response.data;
-                        this.$refs.editazioneTipoLatteModal.open();
-                    });
-
-            });
-
-            $('.delete').click((event) => {
-
-                var element = $(event.currentTarget);
-                this.idTipoLatte = $(element).data("row-id");
-
-                this.$refs.confirmDeleteDialog.open();
-
-            });
-
-        }
-
-
-        // nuova tipoLatte
-        public onAdd() {
-
-            this.tipoLatte = new TipoLatte();
-            this.$refs.editazioneTipoLatteModal.open();
-
-        }
-
-        // rimozione tipoLatte
-        public onRemove() {
-
-            this.tipiLatteService.delete(this.idTipoLatte)
-                .then(response => {
-                    this.$refs.removedDialog.open();
-                });
-        }
-
-
-        // inizializzazione tabella
-        private initTable(): void {
-            var options: any = {};
-            options.columns = [];
-
-            options.columns.push({ data: "Id" });
-            options.columns.push({ data: "Descrizione" });
-            options.columns.push({ data: "DescrizioneBreve" });
-            options.columns.push({ data: "FattoreConversione" });
-
-            var ce = this.canEdit;
-            var cr = this.canRemove;
-
-            if (ce || cr) {
-
-                options.columns.push({
-                    render: function (data: any, type: any, row: any) {
-
-                        var html = '<div class="text-center">';
-
-                        if (ce)
-                            html += '<a class="edit" title="modifica" style="cursor: pointer;" data-row-id="' + row.Id + '" ><i class="far fa-edit"></i></a>';
-
-                        if (cr)
-                            html += '<a class="pl-3 delete" title="elimina" style="cursor: pointer;" data-row-id="' + row.Id + '" ><i class="far fa-trash-alt"></i></a>';
-
-                        html += '</div>';
-
-                        return html;
-                    },
-                    className: "edit-column",
-                    orderable: false
-                });
-
-            }
-            this.tableOptions = options;
-
-        }
-
-    }
-
-
-
+    this.tableOptions = options;
+  }
+}
 </script>
