13fbdee1ab3cb0a021bc6c8c516bbc98b6bc57a0
Giacomo Corallini <corallinigiacomo89@gmail.com>
2020-05-27 09:11:14 +0200

    fix grafici breadcrump + jumbotron

--- lattemarche-vue/src/App.vue
+++ lattemarche-vue/src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="app">
-    <p>Latte Marche</p>  
+    <p>Latte Marche</p>
   </div>
 </template>
 
 <script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 
-import { Component, Vue } from 'vue-property-decorator';
-
-export default class App extends Vue {
-
-}
-
+export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-</style>
