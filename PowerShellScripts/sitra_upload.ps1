dbb693188b711779e772093e971980f979722c6f
Roberto Borsini <roberto.borsini@gmail.com>
2020-08-10 15:17:11 +0200

    #326178

---
+++ PowerShellScripts/sitra_upload.ps1
@@ -0,0 +1,27 @@
+# $url = "https://lattemarche.azurewebsites.net/"
+$url = "http://robertoborsini.myqnapcloud.com:81/"
+$username = "02102002"
+$password = "giorgia2"
+
+# Recupero Token
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/x-www-form-urlencoded")
+
+$body = "grant_type=password&username=$username&password=$password"
+
+$response = Invoke-RestMethod $url'//Token' -Method 'POST' -Headers $headers -Body $body 
+$response | ConvertTo-Json
+
+$accessToken = $response.access_token
+$bearer = "Bearer $accessToken"
+
+# POST Prelievi/Synch
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization",  $bearer)
+
+$response = ""
+$response = Invoke-RestMethod $url'/api/PrelieviLatte/Synch' -Method 'POST' -Headers $headers
+$response | ConvertTo-Json    
+
+
+Write-Output "Done"
