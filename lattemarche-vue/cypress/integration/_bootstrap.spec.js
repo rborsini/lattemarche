228336eb0e592571e766c1fe978fdcddaa76dc92
Roberto Borsini <roberto.borsini@gmail.com>
2020-11-24 17:23:57 +0100

    configurato retry cypress

--- lattemarche-vue/cypress/integration/_bootstrap.spec.js
+++ lattemarche-vue/cypress/integration/_bootstrap.spec.js
@@ -1,6 +1,13 @@
 describe('Bootstrap', () => {
 
-    it('Site open', () => {    
+    var retryObj = {
+        retries: {
+            runMode: 5,
+            openMode: 3
+        }
+    };
+
+    it('Site open', retryObj, () => {    
       
         cy.visit(Cypress.env('url'), { timeout: 600000 });
   
